c08474339a05e274a7a2dd6ccb8047069a872a71
Patrick Rallings <Patrick.Rallings@gmail.com>
2022-05-31 13:51:38 -0400

    Initial commit

---
+++ src/main/java/me/rallings/springbootdatabaseusers/SpringbootDatabaseUsersApplication.java
@@ -0,0 +1,13 @@
+package me.rallings.springbootdatabaseusers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDatabaseUsersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDatabaseUsersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/rallings/springbootdatabaseusers/SpringbootDatabaseUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package me.rallings.springbootdatabaseusers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDatabaseUsersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
